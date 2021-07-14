b5c73783be7affabfa3889f6af7424b39ffcc76c
FredrickSimi <fkemosi@gmail.com>
2021-07-14 14:25:44 +0300

    fourth commit

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # url = "https://covid-193.p.rapidapi.com/countries"
 # querystring = {"search": "kenya"}
 # headers = {
-#     'x-rapidapi-key': "6672acff71msha2668ee10af537bp1245a2jsn31635e6b6758",
+#     'x-rapidapi-key': "",
 #     'x-rapidapi-host': "covid-193.p.rapidapi.com"
 # }
 # response = requests.request("GET", url, headers=headers, params=querystring)
